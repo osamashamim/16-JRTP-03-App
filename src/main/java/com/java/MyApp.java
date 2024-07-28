ec870f14de9aba7d83abedda8aa7f8b664c33ab0
osama <mr.osama1402@gmail.com>
2024-07-28 11:50:21 +0530

    My second commit

--- src/main/java/com/java/MyApp.java
+++ src/main/java/com/java/MyApp.java
@@ -3,7 +3,7 @@ package com.java;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to my Product Store");
+		System.out.println("Welcome to my Product Store....!!!");
 
 	}
 
